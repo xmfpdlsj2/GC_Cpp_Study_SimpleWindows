3ac97af2c03f092c4e80a5d29800970d7ad6bc4e
HoJun Sung <sunghojn@gmail.com>
2023-01-30 17:03:44 +0900

    SW02: Finalize card game. Add player1 and 2 logic and each count stings.

--- SW02_CardMemory/GameLogic.cpp
+++ SW02_CardMemory/GameLogic.cpp
@@ -51,20 +51,42 @@ namespace solitaire
 	}
 	void GameLogic::GameClear()
 	{
+		int p{};
+		std::wostringstream oss;
+
 		if (mStageCount == MAX_ROUND)
 		{
+			if (mMatchCountP1 == mMatchCountP2)
+			{
+				if (mFlipCountP1 == mFlipCountP2)
+				{
+					oss << L"대박! 무승부 입니다!!!!!";
+				}
+				else
+				{
+					p = mFlipCountP1 < mFlipCountP2 ? 2 : 1;
+					oss << L"와우! 카드매칭 수는 같습니다!\n" << 
+						L"하지만 FlipCount 차이로, 최종 승자는 Player " << p << L" 입니다!!";
+				}
+			}
+			else
+			{
+				p = mMatchCountP1 < mMatchCountP2 ? 2 : 1;
+				oss << L"축하합니다! 카드매칭 차이로\n 최종 승자는 Player " << p << L" 입니다!!";
+			}
+
 			MessageBox(NULL,
-				L"축하합니다! 마지막 라운드를 클리어했습니다.",
-				L"Game Clear!",
+				oss.str().c_str(),
+				L"Winer winer chicken dinner!!",
 				MB_OK);
 			PostMessageA(mHwnd, WM_CLOSE, 0, 0);
 			return;
 		}
 
-		int msgBoxId = MessageBox(NULL,
-			L"축하합니다! 다음 라운드를 플레이 하시겠습니까? 아니라면 종료합니다.",
-			L"Game Clear!",
-			MB_YESNO);
+		p = mIsPlayer1 ? 1 : 2;
+		oss << L"이어서 다음 라운드를 플레이 하시겠습니까? 아니라면 종료합니다.\n\n선은 계속해서: "
+			<< L"Player " << p;
+		int msgBoxId = MessageBox(NULL, oss.str().c_str(), L"Game Clear!", MB_YESNO);
 
 		switch (msgBoxId)
 		{
@@ -105,30 +127,47 @@ namespace solitaire
 		{
 			card.Draw(graphics);
 		}
-
-
+		
+		DrawStringRect(graphics);
+	}
+	void GameLogic::DrawStringRect(Gdiplus::Graphics& graphics)
+	{
 		Gdiplus::SolidBrush brush(Gdiplus::Color(200, 200, 200));
 		Gdiplus::Font font(L"맑은 고딕", 20);
 		Gdiplus::StringFormat format;
 		format.SetAlignment(Gdiplus::StringAlignmentNear);
 		format.SetLineAlignment(Gdiplus::StringAlignmentCenter);
 
-		brush.SetColor(Gdiplus::Color(255, 255, 64));
+		brush.SetColor(Gdiplus::Color().LightYellow);
 		std::wostringstream ossS{};
-		ossS << "Stage " << mStageCount;
+		ossS << "Stage " << mStageCount + 1;
 		Gdiplus::PointF pos{ COUNT_RECT_P1.X, 20 };
 		graphics.DrawString(ossS.str().c_str(), -1, &font, pos, &brush);
 
+		Gdiplus::SolidBrush brushP2(Gdiplus::Color(200, 200, 200));
+		if (mIsPlayer1)
+		{
+			brushP2.SetColor(Gdiplus::Color(200, 200, 200));
+			brush.SetColor(Gdiplus::Color().GreenYellow);
+		}
+		else
+		{
+			brush.SetColor(Gdiplus::Color(200, 200, 200));
+			brushP2.SetColor(Gdiplus::Color().GreenYellow);
+		}
 		
-		brush.SetColor(Gdiplus::Color(200, 200, 200));
 		std::wostringstream oss{};
-		oss << "Player1" << L"\n클릭수:" << mFlipCount;
+		oss << "Player1" << L"\n클릭수:" << mFlipCountP1 << L"\n매칭수:" << mMatchCountP1;
 		graphics.DrawString(oss.str().c_str(), -1, &font, COUNT_RECT_P1, &format, &brush);
 
-		brush.SetColor(Gdiplus::Color(200, 200, 200));
 		std::wostringstream oss2{};
-		oss2 << "Player2" << L"\n클릭수:" << mFlipCount;;
-		graphics.DrawString(oss2.str().c_str(), -1, &font, COUNT_RECT_P2, &format, &brush);
+		oss2 << "Player2" << L"\n클릭수:" << mFlipCountP2 << L"\n매칭수:" << mMatchCountP2;
+		graphics.DrawString(oss2.str().c_str(), -1, &font, COUNT_RECT_P2, &format, &brushP2);
+		
+
+		brush.SetColor(Gdiplus::Color().RosyBrown);
+		pos = (Gdiplus::PointF{ COUNT_RECT_P1.X, COUNT_RECT_P2.Y + 150 });
+		graphics.DrawString(L"총 4 Stage", -1, &font, pos, &brush);
 	}
 	void GameLogic::OnClick(int x, int y)
 	{
@@ -144,11 +183,19 @@ namespace solitaire
 
 		if (pCard)
 		{
-			mFlipCount++;
+			if (mIsPlayer1)
+			{
+				mFlipCountP1++;
+			}
+			else
+			{ 
+				mFlipCountP2++;
+			}
+
 			RECT rct = { static_cast<LONG>(COUNT_RECT_P1.GetLeft()),
 				static_cast<LONG>(COUNT_RECT_P1.GetTop()),
 				static_cast<LONG>(COUNT_RECT_P1.GetRight()),
-				static_cast<LONG>(COUNT_RECT_P1.GetBottom()) };
+				static_cast<LONG>(COUNT_RECT_P2.GetBottom()) };
 			InvalidateRect(mHwnd, &rct, false);
 			
 			if (mpSelectedCard == nullptr)
@@ -180,9 +227,10 @@ namespace solitaire
 
 					mpSelectedCard = nullptr;
 
+					mIsPlayer1 ? mMatchCountP1++ : mMatchCountP2++;
+					InvalidateRect(mHwnd, &rct, false);
 					if (mDeck.empty())
 					{
-						mFlipCount = 0;
 						GameClear();
 					}
 				}
@@ -193,8 +241,16 @@ namespace solitaire
 
 					pCard->Flip(false);
 					mpSelectedCard->Flip(false);
-
 					mpSelectedCard = nullptr;
+
+					mIsPlayer1 = !mIsPlayer1;
+					std::wostringstream oss;
+					int p = mIsPlayer1 ? 1 :2;
+					oss << L"플레이어 변경: " << "Player " << p;
+					MessageBox(NULL,
+						oss.str().c_str(),
+						L"Player Change!",
+						MB_OK);
 				}
 			}
 		}
--- SW02_CardMemory/GameLogic.h
+++ SW02_CardMemory/GameLogic.h
@@ -7,14 +7,15 @@ namespace solitaire
 	class GameLogic
 	{
 		const int BOARD_ROW{ 2 }, BOARD_COLUMN{ 5 }, MAX_ROUND{ 3 };
-		const Gdiplus::RectF COUNT_RECT_P1{ 890.0f, 100.0f, 144.0f, 70.0f };
-		const Gdiplus::RectF COUNT_RECT_P2{ 890.0f, 200.0f, 144.0f, 70.0f };
+		const Gdiplus::RectF COUNT_RECT_P1{ 890.0f, 100.0f, 144.0f, 100.0f };
+		const Gdiplus::RectF COUNT_RECT_P2{ 890.0f, 250.0f, 144.0f, 100.0f };
 		
 	private:
 		std::unique_ptr<Gdiplus::Image> mBackground;
 		std::list<Card> mDeck;
 		int mStageCount{};
-		int mFlipCount{};
+		int mFlipCountP1{}, mMatchCountP1{};
+		int mFlipCountP2{}, mMatchCountP2{};
 		bool mIsPlayer1{ true };
 
 		Card* mpSelectedCard{};
@@ -29,6 +30,7 @@ namespace solitaire
 		void Initialize(HWND hwnd);
 		void Release();
 		void Draw(Gdiplus::Graphics& graphics);
+		void DrawStringRect(Gdiplus::Graphics& graphics);
 		void OnClick(int x, int y);
 	};
 }
