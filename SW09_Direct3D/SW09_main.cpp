c42d192938297362609271b4d4c815fde8318b44
HoJun Sung <sunghojn@gmail.com>
2023-02-18 13:25:22 +0900

    SW09 : Finalize basic code of DirectX 3D. Add Full screen mode function.

--- SW09_Direct3D/SW09_main.cpp
+++ SW09_Direct3D/SW09_main.cpp
@@ -8,8 +8,8 @@
 
 const wchar_t gClassName[]{ L"MyWindowClass" };
 const wchar_t gTitle[]{ L"Direct3D Sample" };
-const int WINDOW_WIDTH{ 1024 };
-const int WINDOW_HEIGHT{ 768 };
+int gWindowWidth{ 1024 };
+int gWindowHeight{ 768 };
 
 HWND gHwnd{};
 HINSTANCE gInstance{};
@@ -35,10 +35,15 @@ Microsoft::WRL::ComPtr<ID3D11Texture2D> gspDepthStencil{};
 Microsoft::WRL::ComPtr<ID3D11RenderTargetView> gspRenderTargetView{};
 Microsoft::WRL::ComPtr<ID3D11DepthStencilView> gspDepthStencilView{};
 
+bool gMinimized{ false };
+bool gMaximized{ false };
+bool gResizing{ false };
 
 void InitD3D();
+void OnResize();
 void DestroyD3D();
 void Render();
+
 
 int WINAPI WinMain(
 	_In_ HINSTANCE hInstance,
@@ -66,7 +71,7 @@ int WINAPI WinMain(
 		return 0;
 	}
 
-	RECT wr = { 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT };
+	RECT wr = { 0, 0, gWindowWidth, gWindowHeight };
 	AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);
 	gHwnd = CreateWindowEx(
 		NULL,
@@ -123,6 +128,61 @@ LRESULT WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message)
 	{
+		case WM_ENTERSIZEMOVE:
+			gResizing = true;
+			break;
+		case WM_SIZE:
+			if (gspDevice == nullptr)
+			{
+				return 0;
+			}
+			gWindowWidth = LOWORD(lParam);
+			gWindowHeight = HIWORD(lParam);
+
+			if (wParam == SIZE_MINIMIZED)
+			{
+				gMinimized = true;
+				gMaximized = false;
+			}
+			else if (wParam == SIZE_MAXIMIZED)
+			{
+				gMinimized = false;
+				gMaximized = true;
+				OnResize();
+			}
+			else if (wParam == SIZE_RESTORED)
+			{
+				if (gMinimized)
+				{
+					gMinimized = false;
+					OnResize();
+				}
+				else if (gMaximized)
+				{
+					gMaximized = false;
+					OnResize();
+				}
+				else if (gResizing)
+				{
+					Render();
+				}
+				else
+				{
+					OnResize();
+				}
+			}
+			break;
+		case WM_EXITSIZEMOVE:
+			gResizing = false;
+			OnResize();
+			break;
+		case WM_GETMINMAXINFO:
+			((MINMAXINFO*)lParam)->ptMinTrackSize.x = 640;
+			((MINMAXINFO*)lParam)->ptMinTrackSize.y = 480;
+			break;
+		case WM_MENUCHAR:
+			return MAKELRESULT(0, MNC_CLOSE);
+			break;
 		case WM_CLOSE:
 			DestroyWindow(hwnd);
 			break;
@@ -143,10 +203,13 @@ void InitD3D()
 	ZeroMemory(&scd, sizeof(DXGI_SWAP_CHAIN_DESC));
 	scd.BufferCount = 1;
 	scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	scd.BufferDesc.Width = gWindowWidth;
+	scd.BufferDesc.Height = gWindowHeight;
 	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
 	scd.OutputWindow = gHwnd;
 	scd.SampleDesc.Count = 1;	// 1x MSAA
 	scd.Windowed = TRUE;
+	scd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
 
 	D3D11CreateDeviceAndSwapChain(
 		NULL,
@@ -163,6 +226,23 @@ void InitD3D()
 		gspDeviceContext.ReleaseAndGetAddressOf()
 	);
 
+	OnResize();
+}
+
+void OnResize()
+{
+	ID3D11RenderTargetView* views[]{ nullptr };
+	gspDeviceContext->OMSetRenderTargets(_countof(views), views, nullptr);
+
+	gspRenderTargetView.Reset();
+	gspDepthStencilView.Reset();
+	gspRenderTarget.Reset();
+	gspDepthStencil.Reset();
+
+	gspDeviceContext->Flush();
+	gspSwapChain->ResizeBuffers(0, gWindowWidth, gWindowHeight,
+		DXGI_FORMAT_UNKNOWN, 0);
+
 	gspSwapChain->GetBuffer(0, IID_PPV_ARGS(gspRenderTarget.ReleaseAndGetAddressOf()));
 	gspDevice->CreateRenderTargetView(
 		gspRenderTarget.Get(),
@@ -172,8 +252,8 @@ void InitD3D()
 
 	CD3D11_TEXTURE2D_DESC td(
 		DXGI_FORMAT_D24_UNORM_S8_UINT,
-		WINDOW_WIDTH,
-		WINDOW_HEIGHT,
+		gWindowWidth,
+		gWindowHeight,
 		1,
 		1,
 		D3D11_BIND_DEPTH_STENCIL
@@ -194,6 +274,8 @@ void InitD3D()
 
 void DestroyD3D()
 {
+	gspSwapChain->SetFullscreenState(FALSE, nullptr);
+
 	gspDepthStencilView.Reset();
 	gspDepthStencil.Reset();
 	gspRenderTargetView.Reset();
@@ -209,7 +291,7 @@ void DestroyD3D()
 
 void Render()
 {
-	CD3D11_VIEWPORT viewport(0.0f, 0.0f, static_cast<float>(WINDOW_WIDTH), static_cast<float>(WINDOW_HEIGHT));
+	CD3D11_VIEWPORT viewport(0.0f, 0.0f, static_cast<float>(gWindowWidth), static_cast<float>(gWindowHeight));
 	gspDeviceContext->RSSetViewports(1, &viewport);
 
 	const static float clear_color[4]{ 0.0f, 0.2f, 0.4f, 1.0f };
